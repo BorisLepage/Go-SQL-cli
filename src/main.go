30f592b3b84508a5f8dbb0b66879fc987c5e01bc
Boris Lepage <blepage@synthesio.com>
2023-07-04 21:45:13 +0200

    manage connection to mysql db

---
+++ src/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+    "bufio"
+    "database/sql"
+    "errors"
+    "fmt"
+    "os"
+    "strings"
+
+    "github.com/go-sql-driver/mysql"
+)
+
+func init() {
+    sql.Register("mysql", &mysql.MySQLDriver{})
+}
+
+func main() {
+    args := readPromptArgs()
+
+    err := validatePromptArgs(args)
+    if err != nil {
+        fmt.Println("Erreur lors de la validation des arguments :", err)
+        return
+    }
+
+    db, err := connectToDatabase(args)
+    if err != nil {
+        fmt.Println("Erreur lors de la connexion à la base de données :", err)
+        return
+    }
+    defer db.Close()
+
+    fmt.Printf("Vous êtes connecté à la base de données %s\n", args.Database)
+}
+
+type PromptArgs struct {
+    Hostname string
+    Username string
+    Password string
+    Database string
+}
+
+func validatePromptArgs(args PromptArgs) error {
+    if args.Hostname == "" {
+        return errors.New("Hostname is required")
+    }
+    if args.Username == "" {
+        return errors.New("Username is required")
+    }
+    if args.Database == "" {
+        return errors.New("Database name is required")
+    }
+    return nil
+}
+
+func readPromptArgs() PromptArgs {
+    reader := bufio.NewReader(os.Stdin)
+
+    fmt.Print("Hostname: ")
+    hostname, _ := reader.ReadString('\n')
+    hostname = strings.TrimSpace(hostname)
+
+    fmt.Print("Username: ")
+    username, _ := reader.ReadString('\n')
+    username = strings.TrimSpace(username)
+
+    fmt.Print("Password: ")
+    password, _ := reader.ReadString('\n')
+    password = strings.TrimSpace(password)
+
+    fmt.Print("Database: ")
+    database, _ := reader.ReadString('\n')
+    database = strings.TrimSpace(database)
+
+    args := PromptArgs{
+        Hostname: hostname,
+        Username: username,
+        Password: password,
+        Database: database,
+    }
+
+    return args
+}
+
+func connectToDatabase(args PromptArgs) (*sql.DB, error) {
+    connectionString := fmt.Sprintf("%s:%s@tcp(%s)/%s", args.Username, args.Password, args.Hostname, args.Database)
+
+    db, err := sql.Open("mysql", connectionString)
+    if err != nil {
+        return nil, err
+    }
+
+    err = db.Ping()
+    if err != nil {
+        return nil, err
+    }
+
+    return db, nil
+}
