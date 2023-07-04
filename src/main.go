be70f35613320a4f354b530bb705ffb1c56322d1
Boris Lepage <blepage@synthesio.com>
2023-07-04 21:55:28 +0200

    remove init

--- src/main.go
+++ src/main.go
@@ -8,12 +8,8 @@ import (
     "os"
     "strings"
 
-    "github.com/go-sql-driver/mysql"
+    _ "github.com/go-sql-driver/mysql"
 )
-
-func init() {
-    sql.Register("mysql", &mysql.MySQLDriver{})
-}
 
 func main() {
     args := readPromptArgs()
